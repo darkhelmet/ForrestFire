36135cca214f043e0b38c52f518093492c974664
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-11-27 00:11:08 -0700

    Use the text/template stuff

--- render/render.go
+++ render/render.go
@@ -1,17 +1,18 @@
 package render
 
 import (
+    "bytes"
     "cache"
     "fmt"
     "io/ioutil"
-    "html/template"
+    "text/template"
     "github.com/darkhelmet/web.go"
 )
 
 const TTL = 24 * 60 * 60 * 1e9 // 1 day
 
 func expand(path string) string {
-    return fmt.Sprintf("views/%s.mustache", path)
+    return fmt.Sprintf("views/%s.tmpl", path)
 }
 
 func getViewFile(file string) string {
@@ -19,11 +20,29 @@ func getViewFile(file string) string {
     return string(bytes)
 }
 
+func getTemplate(name string) *template.Template {
+    tmpl, err := template.ParseFile(expand(name))
+    if err != nil {
+        panic(err.Error())
+    }
+    return tmpl
+}
+
+func renderToString(name string, data map[string]string) string {
+    var buffer bytes.Buffer
+    tmpl := getTemplate(name)
+    err := tmpl.Execute(&buffer, data)
+    if err != nil {
+        panic(err.Error())
+    }
+    return buffer.String()
+}
+
 func Page(page string, ctx *web.Context) string {
     yield := Chunk(page)
-    footer := Chunk(page)
-    return cache.CheckAndSet(page, TTL, func() cache.Any {
-        return mustache.RenderFile(expand("layout"), map[string]string{
+    footer := Chunk("footer")
+    return cache.CheckAndSet(fmt.Sprintf("page/%s", page), TTL, func() cache.Any {
+        return renderToString("layout", map[string]string{
             "yield":  yield,
             "donate": getViewFile("donate"),
             "footer": footer,
@@ -33,8 +52,8 @@ func Page(page string, ctx *web.Context) string {
 }
 
 func Chunk(chunk string) string {
-    return cache.CheckAndSet(chunk, TTL, func() cache.Any {
-        return mustache.RenderFile(expand(chunk), map[string]string{
+    return cache.CheckAndSet(fmt.Sprintf("chunk/%s", chunk), TTL, func() cache.Any {
+        return renderToString(chunk, map[string]string{
             "donate": getViewFile("donate"),
         })
     }).(string)
