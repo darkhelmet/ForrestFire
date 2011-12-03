a8c6b462d8ff69ba4624541518d7c9d6524e4deb
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-12-03 00:50:38 -0700

    Handle some mc errors

--- cache/memcache.go
+++ cache/memcache.go
@@ -19,23 +19,30 @@ func newMemcacheCache(server, username, password string) (c *mcCache) {
     if cn, err := mc.Dial("tcp", fmt.Sprintf("%s:11211", server)); err != nil {
         panic(err.Error())
     } else {
-        if err = cn.Auth(username, password); err != nil {
-            panic(err.Error())
-        } else {
-            c = &mcCache{cn, username, password}
-        }
+        c = &mcCache{cn, username, password}
+        c.auth()
     }
     return
 }
 
 func (c *mcCache) auth() {
-    
+    if err := c.conn.Auth(c.username, c.password); err != nil {
+        panic(err.Error())
+    }
+}
+
+func (c *mcCache) handleError(action, key string, err error) {
+    if err == mc.ErrAuthRequired {
+        c.auth()
+    } else {
+        log(action, key, err)
+    }
 }
 
 func (c *mcCache) Get(key string) (string, error) {
     value, _, _, err := c.conn.Get(key)
     if err != nil {
-        log("get", key, err)
+        c.handleError("get", key, err)
     }
     return value, err
 }
@@ -43,19 +50,19 @@ func (c *mcCache) Get(key string) (string, error) {
 func (c *mcCache) Set(key, data string, ttl int) {
     // Don't worry about errors, live on the edge
     if err := c.conn.Set(key, data, 0, 0, ttl); err != nil {
-        log("set", key, err)
+        c.handleError("set", key, err)
     }
 }
 
 func (c *mcCache) Fetch(key string, ttl int, f func() string) string {
     value, cas, _, err := c.conn.Get(key)
     if err != nil {
-        log("fetch/get", key, err)
+        c.handleError("fetch/get", key, err)
         value = f()
         /*  If this fails, don't worry too much
             In the situations it gets used, it doesn't matter */
         if err = c.conn.Set(key, value, cas, 0, ttl); err != nil {
-            log("fetch/set", key, err)
+            c.handleError("fetch/set", key, err)
         }
     }
     return value
